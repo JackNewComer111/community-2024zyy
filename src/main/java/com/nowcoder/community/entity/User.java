63f36861edeb569e72f4798482e9736a44913f8e
zyy <1400034431@qq.com>
2024-04-09 22:04:32 +0800

    community

---
+++ src/main/java/com/nowcoder/community/entity/User.java
@@ -0,0 +1,40 @@
+package com.nowcoder.community.entity;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * ClassName: User
+ * Package: com.nowcoder.community.entity
+ * Description:
+ *
+ * @Autuor Dongjie Sang
+ * @Create 2023/5/25 20:37
+ * @Version 1.0
+ */
+@Data
+public class User {
+
+    private int id;
+    @NotBlank(message = "用户名不能为空！")
+    @Size(min = 3,max = 15,message = "用户名长度必须在3到15位之间！")
+    private String username;
+    @NotBlank(message = "密码不能为空！")
+    @Size(min = 8,max = 20,message = "密码长度必须在8到20位之间！")
+    private String password;
+    private String salt;
+    @NotBlank(message = "邮箱不能为空！")
+    @Email(message = "请注意邮箱格式！")
+    private String email;
+    private int type;
+    private int status;
+    private String activationCode;
+    private String headerUrl;
+    private Date createTime;
+
+}
---
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -0,0 +1,32 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectPosts() {
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149,0,10);
+        for (DiscussPost post : list) {
+            System.out.println(post);
+        }
+
+        int rows = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
+    }
+
+}
